93088e0cc9d2614870eaff40bfd6eb4a06f316a8
caioc <caioccoutinho2301@gmail.com>
2025-09-17 20:32:25 -0300

    Projeto Angular 19 - Barbearia com MainComponent

--- angular-app/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: '<app-main></app-main>'
-})
-export class AppComponent {
-  title = 'angular-app';
-}
