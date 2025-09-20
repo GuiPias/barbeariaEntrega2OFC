184cab957f4b69c781cc991caa494ab47a60737d
caioc <caioccoutinho2301@gmail.com>
2025-09-19 23:23:25 -0300

    cliente esta salvando apenas na array (ainda n salva no banco)

--- barbearia-app/src/app/components/clientes/clientes.component.ts
+++ barbearia-app/src/app/components/clientes/clientes.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-clientes',
